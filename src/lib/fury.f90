e844efdf043722cc25ee04836a3854c07ef87dbb
Stefano Zaghi <stefano.zaghi@gmail.com>
2016-08-08 17:33:22 +0200

    Add a basic definition of metre
    Add a basic definition of metre to test the classes hierarchy:
    
    + unit abstract
        + unit metre is a concrete extension of unit abstract
            + unit kilometre will be an extension of metre
    + units SI is a collection of instances of concrete units

--- src/lib/fury.f90
+++ src/lib/fury.f90
@@ -5,13 +5,27 @@ module fury
 !-----------------------------------------------------------------------------------------------------------------------------------
 use fury_qinteger, only : qinteger
 use fury_qreal, only : qreal
-! use fury_units_si
+use fury_units_si, only : initialize_si=>initialize, metre
+use penf
 !-----------------------------------------------------------------------------------------------------------------------------------
 
 !-----------------------------------------------------------------------------------------------------------------------------------
 implicit none
 private
+! quantity with unit
 public :: qinteger
 public :: qreal
+! SI units
+public :: initialize_si
+public :: metre
+! kinds
+public :: R8P
+public :: R4P
+public :: R_P
+public :: I8P
+public :: I4P
+public :: I2P
+public :: I1P
+public :: I_P
 !-----------------------------------------------------------------------------------------------------------------------------------
 endmodule fury
